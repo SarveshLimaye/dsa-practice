6d9365f06ca89dc7b76e0dabfc09f9e2d0dda338
SarveshLimaye <sarveshlimaye2002@gmail.com>
2021-09-25 22:08:29 +0530

    leetcode question number 268

---
+++ binarySearch/missingNumberArr.java
@@ -0,0 +1,40 @@
+//leetcode question - https://leetcode.com/problems/missing-number/
+
+package com.binarySearch;
+
+public class missingNumberArr {
+    public static void main(String[] args) {
+        int[] nums = {3,0,1};
+        int ans = missingNumber(nums);
+        System.out.println(ans);
+    }
+   static int missingNumber(int[] arr) {
+        int i = 0;
+        while(i < arr.length){
+            int correct = arr[i];
+            if(arr[i]<arr.length && arr[i] != arr[correct]){
+                swap(arr,i,correct);
+            }else{
+                i++;
+            }
+        }
+
+        for(int j=0; j<arr.length;j++){
+            if(arr[j] != j){
+                return j;
+            }
+        }
+
+        return arr.length;
+
+
+    }
+
+    static void swap(int[] arr,int first,int second){
+        int temp =  arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+
+}
