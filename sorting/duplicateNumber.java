d59900fdcfc72dbc95cb33ebdbae3f9067ad3ad1
SarveshLimaye <sarveshlimaye2002@gmail.com>
2021-09-27 22:11:29 +0530

    Letcode question 287

---
+++ sorting/duplicateNumber.java
@@ -0,0 +1,34 @@
+package com.sarvesh;
+
+//leetcode question - https://leetcode.com/problems/find-the-duplicate-number/
+
+public class duplicateNumber {
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,2,2};
+        int ans =findDuplicate(arr);
+        System.out.println(ans);
+
+    }
+
+    static int findDuplicate(int[] nums) {
+        int i =0;
+        while(i < nums.length){
+            int correct = nums[i] -1;
+            if(nums[i] != nums[correct]){
+                swap(nums,i,correct);
+            }else{
+                i++;
+            }
+        }
+
+        return nums[nums.length -1];
+
+    }
+
+    static void swap(int[] arr,int first,int second){
+        int temp =  arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+}
