8a4b751c3b7359726006a32477eabaef49bd47db
SarveshLimaye <sarveshlimaye2002@gmail.com>
2021-09-30 17:14:12 +0530

    factorial using recursion

---
+++ recursion/Factorial.java
@@ -0,0 +1,21 @@
+package com.recursion;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.print("Enter number : ");
+        int num;
+        Scanner in = new Scanner(System.in);
+        num= in.nextInt();
+        int ans = fact(num);
+        System.out.println(num+"! = " + ans);
+    }
+
+    static int fact(int x){
+        if (x==1){
+            return 1;
+        }
+        return x*fact(x-1);
+    }
+}
