597ab2068902f3504d8415b287313ce686b5f2de
SarveshLimaye <sarveshlimaye2002@gmail.com>
2021-10-16 21:55:32 +0530

    linear search using recursion

---
+++ recursion/linear.java
@@ -0,0 +1,28 @@
+package com.sarvesh;
+
+import java.util.ArrayList;
+
+public class linear {
+    public static void main(String[] args) {
+        int[] nums = {2,4,7,7,9,11};
+
+        System.out.println(multipleIndex(nums,7,0,new ArrayList<>()));
+    }
+
+    static ArrayList<Integer> multipleIndex(int[] arr, int target,int index,ArrayList<Integer> list){
+        if (index== arr.length){
+            return list;
+        }
+
+        if (arr[index]==target){
+            list.add(index);
+        }
+
+        return multipleIndex(arr,target,index+1,list);
+    }
+
+
+
+
+   
+}
