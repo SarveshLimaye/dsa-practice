b88533f1ae36f3bcf6c84576e8fff89ea8324481
SarveshLimaye <sarveshlimaye2002@gmail.com>
2021-10-23 15:48:12 +0530

    selection sort using recursion

---
+++ recursion/Selection.java
@@ -0,0 +1,28 @@
+package com.recursion;
+
+import java.util.Arrays;
+
+public class Selection {
+    public static void main(String[] args) {
+        int[] arr = {6,8,3,1};
+        selectionSort(arr, arr.length, 0,0);
+        System.out.println(Arrays.toString(arr));
+    }
+
+   static void selectionSort(int[] arr,int r,int c,int max){
+        if(r==0){
+            return;
+        }if (r > c){
+            if(arr[c] > arr[max]){
+                selectionSort(arr,r,c+1,c);
+            }else{
+                selectionSort(arr,r,c+1,max);
+            }
+       }else{
+            int temp = arr[max];
+            arr[max] = arr[r -1];
+            arr[r-1] = temp;
+            selectionSort(arr,r-1,0,0);
+       }
+   }
+}
