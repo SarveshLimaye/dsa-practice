7ef111617e3f934dfcd3fae2a6e3f4cf42f41d7b
SarveshLimaye <sarveshlimaye2002@gmail.com>
2021-10-28 22:26:38 +0530

    skip char or string

---
+++ recursion/Skip.java
@@ -0,0 +1,37 @@
+package com.recursion;
+
+public class Skip {
+    public static void main(String[] args) {
+      String ans ="badcftra";
+      System.out.println(SkipChar(ans));
+      String ans1 = "loitadkjhyuiadk";
+      System.out.println(SkipString(ans1));
+
+    }
+
+    static String SkipChar(String input){
+          if(input.isEmpty()){
+             return "";
+          }
+
+          char ch = input.charAt(0);
+
+          if(ch=='a'){
+           return SkipChar(input.substring(1));
+          }else{
+              return ch+SkipChar(input.substring(1));
+          }
+    }
+
+    static String SkipString(String input){
+        if(input.isEmpty()){
+            return "";
+        }
+       if(input.startsWith("adk")){
+           return SkipString(input.substring(3));
+       }else {
+           return input.charAt(0) + SkipString(input.substring(1));
+       }
+
+    }
+}
