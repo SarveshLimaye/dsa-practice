ade19421e3422a9f2532502f7502bf47bf571f09
SarveshLimaye <sarveshlimaye2002@gmail.com>
2021-11-06 21:09:09 +0530

    reverse triangle pattern using recursion

---
+++ recursion/Triangle.java
@@ -0,0 +1,20 @@
+package com.recursion;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Pattern(4,0);
+
+    }
+    static void Pattern(int r,int c){
+        if(r==0){
+            return;
+        }
+        if( c < r){
+            System.out.print("*");
+            Pattern(r,c+1);
+        }else {
+            System.out.println();
+            Pattern(r-1,0);
+        }
+    }
+}
