cd879688cb7f5b55f48edefdb8fb750da503d4e6
SarveshLimaye <sarveshlimaye2002@gmail.com>
2021-11-14 22:09:25 +0530

    sum digit easy q

---
+++ recursion/SumDigit.java
@@ -0,0 +1,14 @@
+package com.recursion;
+
+public class SumDigit {
+    public static void main(String[] args) {
+        System.out.println(sum(12));
+    }
+    static int sum(int num){
+        if(num==0){
+            return 0;
+        }
+
+        return (num%10) + sum(num/10);
+    }
+}
